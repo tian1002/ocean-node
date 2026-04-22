bee753f0d508d7415852fd7da149526eafb97d9b
denisiuriet <iuriet.denis@gmail.com>
2026-04-22 11:54:21 +0300

    extra validation

--- src/components/c2d/compute_engine_docker.ts
+++ src/components/c2d/compute_engine_docker.ts
@@ -405,9 +405,11 @@ export class C2DEngineDocker extends C2DEngine {
         let isBenchmarkEnv = false
         if (env.access?.accessLists) {
           const baseAccessList = env.access?.accessLists?.[0] as AccessList
-          isBenchmarkEnv = baseAccessList[BASE_CHAIN_ID].includes(
-            getAddress('0xcb7Db55Ca9Aa9C3b25F5Bc266da63317fa02086a')
-          )
+          if (baseAccessList[BASE_CHAIN_ID]) {
+            isBenchmarkEnv = baseAccessList[BASE_CHAIN_ID].includes(
+              getAddress('0xcb7Db55Ca9Aa9C3b25F5Bc266da63317fa02086a')
+            )
+          }
         }
 
         if (isBenchmarkEnv) {
